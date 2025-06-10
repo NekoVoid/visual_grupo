a1736d4054390235e7b1bbfb09c166d143f2804e
Juancho Rodriguez <juancho.chiquiza12@gmail.com>
2025-06-10 01:50:47 -0500

    finnished threejs assignment

--- 2025-06-08_taller_cinematica_directa_fk/threejs/src/App.jsx
+++ 2025-06-08_taller_cinematica_directa_fk/threejs/src/App.jsx
@@ -1,84 +1,102 @@
 import * as THREE from "three";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import { useControls } from "leva";
 import { useEffect, useRef, useState } from "react";
 
 
-// This is a simple arm with two segments (shoulder and elbow) that can be rotated
-// independently. The arm is made up of two boxes, one for the shoulder and one for the elbow
-// The shoulder is the parent of the elbow, so when the shoulder rotates, the elbow follows.
+function clamp(a, min, max){
+  return Math.max(min, Math.min(max, a))
+}
+
 function Arm(props){
-  const shoulder = useRef();
-  const elbow = useRef();
+  const baseRef = useRef();
+  const artARef = useRef();
+  const artBRef = useRef();
+  const tipRef = useRef();
 
-  // Original position, rotation and scale of the shoulder and elbow
-  const ogShoulder = {
-    position: new THREE.Vector3(0,0,0),
-    rotation: new THREE.Euler(0,0,0, "XYZ"),
-    scale: new THREE.Vector3(1,1,1),
-  };
-  const ogElbow = {
-    position: new THREE.Vector3(0,-3,0),
-    rotation: new THREE.Euler(0,0,0, "XYZ"),
-    scale: new THREE.Vector3(1,1,1),
-  };
+  const lineRef = useRef();
+  const linePoints = useRef([]);
 
-  // This effect is used to set the position, rotation and scale of the shoulder and elbow, parameters passed as props
-  useEffect(() => {
-    if (props.shoulder){
-      // if shoulder position prop is passed, add it to the original position, set it to the original otherwise
-      if(props.shoulder.position) shoulder.current.position.addVectors(props.shoulder.position, ogShoulder.position);
-      else shoulder.current.position.copy(ogShoulder.position);
-      // if shoulder scale prop is passed, multiply it with the original scale, set it to the original otherwise
-      if(props.shoulder.scale) shoulder.current.scale.multiplyVectors(props.shoulder.scale, ogShoulder.scale);
-      else shoulder.current.scale.copy(ogShoulder.scale);
-      // if shoulder rotation prop is passed, set it to the rotation prop, set it to the original otherwise
-      if(props.shoulder.rotation) shoulder.current.rotation.copy(props.shoulder.rotation);
-      else shoulder.current.rotation.copy(ogShoulder.rotation);
+  const lengthArtA = 4;
+  const lengthArtB = 4;
+
+  useFrame((state, delta) => {
+    const t = state.clock.getElapsedTime();
+    const {factor = {}, displacement = {}}= props;
+
+    baseRef.current.rotation.y = Math.sin(t)*Math.PI*(factor.base ?? 1) + (displacement.base ?? 0);
+    artARef.current.rotation.z = clamp(Math.sin(t)*Math.PI*0.25*(factor.artA ?? 1) + (displacement.artA ?? 0), -Math.PI/4, Math.PI/4);
+    artBRef.current.rotation.z = clamp(Math.sin(t)*Math.PI*0.5*(factor.artB ?? 1) + (displacement.artB ?? 0), -Math.PI/2, Math.PI/2);
+
+    if(t < 2){
+      const worldPos = tipRef.current.localToWorld(new THREE.Vector3(0,1,0));
+      linePoints.current.push(worldPos);
+      lineRef.current.geometry = new THREE.BufferGeometry().setFromPoints(linePoints.current);
+    }else{
+      const worldPos = tipRef.current.localToWorld(new THREE.Vector3(0,1,0));
+
+      linePoints.current.push(worldPos);
+      linePoints.current.shift();
+
+      lineRef.current.geometry = new THREE.BufferGeometry().setFromPoints(linePoints.current);
     }
 
-    if(props.elbow){
-      // same as with the shoulder for the elbow
-      if(props.elbow.position) elbow.current.position.addVectors(props.elbow.position, ogElbow.position);
-      else elbow.current.position.copy(ogElbow.position);
-      if(props.elbow.scale) elbow.current.scale.multiplyVectors(props.elbow.scale, ogElbow.scale);
-      else elbow.current.scale.copy(ogElbow.scale);
-      if(props.elbow.rotation) elbow.current.rotation.copy(props.elbow.rotation);
-      else elbow.current.rotation.copy(ogElbow.rotation);
-    }
-  }, [props.shoulder, props.elbow]);
+  })
 
   return (
-    <group ref={shoulder}>
-      <mesh position={[0, -1.5, 0]}>
-        <boxGeometry args={[1, 3, 1]} />
-        <meshStandardMaterial color="red" />
+    <>
+    <line ref={lineRef}>
+      <lineBasicMaterial color={"orange"}/>
+    </line>
+    <group ref={baseRef}>
+      <mesh position={[0,-0.5,0]}>
+        <cylinderGeometry args={[2,2,1,8]}/>
+        <meshStandardMaterial color="blue"/>
       </mesh>
-      <group ref={elbow} position={[0, -3, 0]}>
-        <mesh position={[0, -1.5, 0]}>
-          <boxGeometry args={[1, 3, 1]} />
-          <meshStandardMaterial color="blue" />
+      <group ref={artARef}>
+        <mesh rotation={new THREE.Euler(Math.PI/2, 0, 0)}>
+          <cylinderGeometry args={[0.6,0.6,,12]}/>
+          <meshStandardMaterial color="green"/>
         </mesh>
+        <mesh position={[0,lengthArtA/2,0]}>
+          <boxGeometry args={[0.8,lengthArtA,0.9]}/>
+          <meshStandardMaterial color={"red"}/>
+        </mesh>
+        <group ref={artBRef} position={[0,lengthArtA,0]}>
+          <mesh rotation={new THREE.Euler(Math.PI/2, 0, 0)}>
+            <cylinderGeometry args={[0.6,0.6,,12]}/>
+            <meshStandardMaterial color="green"/>
+          </mesh>
+          <mesh position={[0,lengthArtB/2,0]}>
+            <boxGeometry args={[0.8,lengthArtB,0.9]}/>
+            <meshStandardMaterial color={"red"}/>
+          </mesh>
+          <group ref={tipRef} position={[0,lengthArtB+0.5,0]}>
+            <mesh>
+              <coneGeometry args={[1,2,6]}/>
+              <meshStandardMaterial color={"purple"}/>
+            </mesh>
+          </group>
+        </group>
       </group>
     </group>
+    </>
   )
 }
 
 export default function App() {
-
-  // articulations transformation state
-  const [shoulder, setShoulder] = useState({
-    position: new THREE.Vector3(0,2.5,0),
-    rotation: new THREE.Euler(0,0,0, "XYZ"),
-    scale: new THREE.Vector3(1,1,1),
+  const {
+    base_Factor, artA_Factor, artB_Factor,
+    base_Displacement, artA_Displacement, artB_Displacement
+   } = useControls({
+    base_Factor: {value:0.5, min:0, max:1, step:0.05},
+    artA_Factor: {value:1, min:0, max:1, step:0.05},
+    artB_Factor: {value:1, min:0, max:1, step:0.05},
+    base_Displacement: {value:0, min:-Math.PI, max:Math.PI, step:0.05},
+    artA_Displacement: {value:0, min:-Math.PI/4, max:Math.PI/4, step:0.05},
+    artB_Displacement: {value:0, min:-Math.PI/2, max:Math.PI/2, step:0.05}
   });
-  const [elbow, setElbow] = useState({
-    position: new THREE.Vector3(0,0,0),
-    rotation: new THREE.Euler(0,0,0, "XYZ"),
-    scale: new THREE.Vector3(1,1,1),
-  });
-
-
 
   return (
     <div style={{ width: "100vw", height: "100vh", display: "grid" }}>
@@ -96,7 +114,7 @@ export default function App() {
           <directionalLight position={[0, 0.5, 1]} intensity={1}/>
           <directionalLight position={[0, 1, 0]} intensity={1}/> 
           {/* Arm component */}
-            <Arm elbow={elbow} shoulder={shoulder}/>
+            <Arm factor={{base: base_Factor, artA: artA_Factor, artB: artB_Factor}} displacement={{base: base_Displacement, artA: artA_Displacement, artB: artB_Displacement}}/>
           <OrbitControls makeDefault/>
         </Canvas>
       </div>
@@ -109,23 +127,6 @@ export default function App() {
           display: "grid"
         }}
       >
-        {/* Elbow component rotation control */}
-        <label>
-          Elbow: <wbr/>
-          <input type="range" min={0} max={5*Math.PI/6} step={0.01} defaultValue={0} onChange={e => {
-            const value = parseFloat(e.target.value);
-            setElbow(prev => ({...prev, rotation: new THREE.Euler(prev.rotation.x, prev.rotation.y, value)}));
-          }}/>
-        </label>
-        
-        {/* Shoulder component rotation control */}
-        <label>
-          Shoulder: <wbr/>
-          <input type="range" min={-Math.PI/2} max={Math.PI/2} step={0.01} defaultValue={0} onChange={e => {
-            const value = parseFloat(e.target.value);
-            setShoulder(prev => ({...prev, rotation: new THREE.Euler(prev.rotation.x, prev.rotation.y, value)}));
-          }}/>
-        </label>
       </div>
     </div>
   );
